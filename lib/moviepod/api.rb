f017dcecfe60d1d927b858b667d6b50ab2b4fab4
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-11-25 08:00:08 +0000

    updating the movie.rb

--- lib/moviepod/api.rb
+++ lib/moviepod/api.rb
@@ -2,11 +2,13 @@ class MoviePod::API
 
   
   def self.trending_movies(user_input)
-    results = RestClient.get("https://api.themoviedb.org/3/trending/#{user_input}/day?api_key=232880aa87c7a2eb09863ce37088d60a")
+    base_url = "https://api.themoviedb.org/3"
+    
+    results = RestClient.get("#{base_url}/trending/#{user_input}/day?api_key=232880aa87c7a2eb09863ce37088d60a")
     json = JSON.parse(results)["results"]
     
-    json.each do |user_hash|
-      media_title = user_hash["title"]
+    json.each do |trend_hash|
+      MoviePod::Movie.new[trend_hash]
       
       binding.pry
     end
--- lib/moviepod/movie.rb
+++ lib/moviepod/movie.rb
@@ -1,7 +1,18 @@
 class MoviePod::Movie 
-  attr_accessor :id, :title, :vote_average
   
-  def initialize(args={})
-    
+  attr_accessor :id, :title, :vote_average, :popularity
+  
+  @@all = []
+  
+  def initialize(args)
+    args.each do |key,value|
+      self.send("#{key}=", value) if self.respond_to?(key)
+    end
+    @@all << self
   end
+  
+  def self.all
+    @@all
+  end
+  
 end
