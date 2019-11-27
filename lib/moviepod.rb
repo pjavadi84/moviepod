1056e30d46435100d3e0e69b4cbd84853c596ae3
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-11-27 01:13:27 +0000

    last updated

--- lib/moviepod.rb
+++ lib/moviepod.rb
@@ -6,5 +6,6 @@ require_relative "moviepod/version"
 require_relative "moviepod/cli"
 require_relative "moviepod/movie"
 require_relative "moviepod/api"
+require_relative "moviepod/tv"
 
 
--- lib/moviepod/cli.rb
+++ lib/moviepod/cli.rb
@@ -8,35 +8,37 @@ class MoviePod::CLI
     puts "Type movie, tv, or all to view the top 20s trending today: "
     user_input = gets.strip
     
-    if user_input == "movie"
-      MoviePod::API.trending_movies(user_input)
-      MoviePod::Movie.all.each.with_index(1) do |movie, index|
-        puts "#{index}- #{movie.title}"
-      end
-      puts "Type the number assoicated with that movie on the list above to read more details about it: "
-      user_input = gets.strip
-    
-      movie = MoviePod::Movie.all[user_input.to_i - 1] 
-      MoviePod::API.single_trending_movie(movie)
-      puts "Overview: #{movie.overview}"
-      puts "Popularity: #{movie.popularity}"
-      puts "Release date: #{movie.release_date}"
-      
-    elsif user_input == "tv" 
+    if user_input == "tv" 
       MoviePod::API.trending_tv(user_input)
       MoviePod::TV.all.each.with_index(1) do |tv, index|
         puts "#{index}- #{tv.name}"
       end
       puts "Type the number assoicated with that TV show on the list above to read more details about it: "
       user_input = gets.strip
-      tv = MoviePod::TV.all[user_input.to_i -1]
-      MoviePod::API.single_trending_tv(tv)
-      puts "Overview: #{tv.overview}"
-      puts "Popularity: #{tv.popularity}"
+        tv = MoviePod::TV.all[user_input.to_i - 1]
+        MoviePod::API.single_trending_tv(tv)
+        # binding.pry
+        puts "@@@@@@#{tv.name}@@@@@@"
+        puts "Overview: #{tv.overview}"
+        puts "Popularity: #{tv.popularity}"
+      
+      
+    
+    elsif user_input == "movie"
+      MoviePod::API.trending_movies(user_input)
+      MoviePod::Movie.all.each.with_index(1) do |movie, index|
+        puts "#{index}- #{movie.title}"
+      end
+      puts "Type the number assoicated with that movie on the list above to read more details about it: "
+      user_input = gets.strip
+      movie = MoviePod::Movie.all[user_input.to_i - 1] 
+      MoviePod::API.single_trending_movie(movie)
+      puts "@@@@@@#{movie.title}@@@@@@"
+      puts "Overview: #{movie.overview}"
+      puts "Popularity: #{movie.popularity}"
+      puts "Release date: #{movie.release_date}"
     end
     
   end
       
-  
-
 end
